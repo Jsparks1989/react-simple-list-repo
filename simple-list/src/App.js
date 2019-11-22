31736b09482cf03526e868ebc3a1be65a34c6ebc
Jsparks1989 <jsparks19891@gmail.com>
2019-11-22 01:23:15 -0500

    Working on 'adding items to the list' functionality. Having trouble with displaying list items. Posted a stackoverflow question to find answers.

--- simple-list/src/App.js
+++ simple-list/src/App.js
@@ -1,26 +1,98 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react';
+import {Modal, Button, Form} from 'react-bootstrap';
+
+
 
 function App() {
+
+  /*============================================================================================
+                              Modal Window state and functions
+  =============================================================================================*/
+  const [show, setShow] = useState(false);
+
+  const changeOpen = () => setShow(true);
+  const changeClose = () => setShow(false);
+
+
+  /*============================================================================================
+                                   List state and functions
+  =============================================================================================*/
+  const [newListItem, setNewListItem] = useState('');
+
+  // Adding to the list
+  var addToList = (e) => {
+    // Stop the page from reloading when form is submitted
+    e.preventDefault();
+    // Add newListItem to list array when form is submitted
+    listItems.push(newListItem);
+    console.log('From addToList: ');
+    console.log(newListItem);
+    console.log('listItems array: ');
+    console.log(listItems);
+  }
+
+  // updateList captures text input from 
+  var updateList = (e) => {
+    //console.log(e.target.value);
+    setNewListItem(e.target.value);
+    console.log('From updateList: ');
+    console.log(newListItem);
+    e.preventDefault();
+  }
+
+  var listItems = [];
+
+
+  /*============================================================================================
+                                   Component return
+  =============================================================================================*/
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
+      <h2>Simple List</h2>
+
+      <Button onClick={changeOpen}>
+        Add to the List
+      </Button>
+
+
+      <Modal show={show} onHide={changeClose}>
+        <Modal.Header closeButton>
+          <Modal.Title>This is the Title</Modal.Title>
+        </Modal.Header>
+              <form onSubmit={addToList}>
+                <Modal.Body>               
+                    <Form.Group>
+                      <Form.Label>Item</Form.Label>
+                      <br />
+                      {/* When user enters text in Form.Control, updateList captures the text. */}
+                      <Form.Control type="text" placeholder="Normal text" onChange={updateList} />
+                    </Form.Group>
+                </Modal.Body>
+                    <Modal.Footer>
+                    <Button type="sumbit">Add to List</Button>
+                    </Modal.Footer>
+              </form>
+      </Modal>
+
+
+
+      {/* List goes here */}
+      <ul>
+        {listItems.map(item =>(
+          <li>{item}</li>
+        ))}
+      </ul>
+
+
     </div>
   );
 }
 
 export default App;
+
+
+
+
